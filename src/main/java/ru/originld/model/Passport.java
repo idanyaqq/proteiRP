ec39d1bcfe01112b9e3de0ed81785a64f8dfb736
Danya <danucemo994@gmail.com>
2017-05-10 00:02:58 +0300

    added Passport class

--- src/main/java/ru/originld/model/Passport.java
+++ src/main/java/ru/originld/model/Passport.java
@@ -1,7 +1,56 @@
 package ru.originld.model;
 
+import javax.persistence.*;
+
 /**
  * Created by Danya on 09/05/2017.
  */
+@Entity
+@Table(name = "passports")
 public class Passport {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nubmer")
+    private long number;
+
+    @Column(name = "address")
+    private String address;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
